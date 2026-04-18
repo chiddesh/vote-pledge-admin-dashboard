94fba57bd301273f2febd16267523eefea4f6a71
chiddesh <chiddesh@gmail.com>
2026-04-18 14:52:27 +0530

    DATA PROGRESS, PAGINATION

--- src/Services/apiPledge.js
+++ src/Services/apiPledge.js
@@ -1,9 +1,5 @@
-import {supabase} from "./Supabase"
-export const filterPledge = async(filters={})=>
-{
-let query = supabase.from("voters").select("*")
+import { supabase } from "./Supabase"
 
-<<<<<<< HEAD
 export const filterPledge = async (filters = {}, page = 1) => {
 
     const limit = 100
@@ -99,54 +95,4 @@ export const getStats = async () => {
         todayCount,
         completion
     }
-=======
-if(filters.gender){
-query=query.eq("gender",filters.gender)
-}
-
-if(filters.area_type){
-query=query.eq("area_type",filters.area_type)
-}
-/* AGE */
-
-if (filters.age === "18-25") {
-query=query.gte("age", 18).lte("age", 25)
-}
-if (filters.age=== "26-40") {
-query=query.gte("age", 26).lte("age", 40)
-}
-if (filters.age === "40+") {  
-    query = query.gt("age", 40)  
-}  
-
-/* CATEGORY */  
-if(filters.category){  
-    query = query.eq("category",filters.category)  
-}  
-
-
-/* COMPLETION */  
-if(filters.Completion === "Completed"){  
-    query = query.eq("will_vote", true)  
-}  
-
-if(filters.Completion === "Not Completed"){  
-    query = query.eq("will_vote", false)  
-}  
-
-
-/* FIRST TIME VOTER */  
-if(filters.first_time_voter){  
-    query = query.eq("first_time_voter", filters.first_time_voter)  
-}  
-
-/* ULB */  
-if(filters.ulb){  
-    query = query.eq("ulb",filters.ulb)  
-}  
-
-/* BLOCK */  
-if(filters.block){  
-    query = query.eq("block",filters.block)
->>>>>>> 420b5dd99cda723a5b377ef3562648a8ce1baf69
 }
